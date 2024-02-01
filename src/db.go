2f1140194356c93f635cc18805b51d7705213e23
mail@lenblum.de <mail@lenblum.de>
2024-02-01 18:59:41 +0100

    initial commit

---
+++ src/db.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var crypto = []byte{}
+
+var (
+	newFile *os.File
+	err     error
+)
+
+func addDocUnsafe(data map[string]string, name string, dir string) error {
+	toJSON := "{"
+	for k, v := range data {
+		toJSON = toJSON + "\"" + k + "\":\"" + v + "\","
+	}
+	toJSON = toJSON[:len(toJSON)-1] + "}"
+	//create document directory if it doesn't exist
+	var document *os.File
+	var err error
+	if dir == "" {
+		_ = os.Mkdir("documents", os.ModePerm)
+		document, err = os.Create("documents/" + name + ".json")
+	} else {
+		_ = os.Mkdir("documents", os.ModePerm)
+		_ = os.Mkdir("documents/"+dir, os.ModePerm)
+		document, err = os.Create("documents/" + dir + "/" + name + ".json")
+	}
+	if err != nil {
+		document.Close()
+		return err
+	}
+	_, err = document.WriteString(toJSON)
+	document.Close()
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func readDocUnsafe(name string, dir string) (map[string]string, error) {
+	if !fileExists("documents/" + dir + "/" + name + ".json") {
+		return nil, os.ErrNotExist
+	}
+	document, err := os.Open("documents/" + name + ".json")
+	if err != nil {
+		return nil, err
+	}
+	defer document.Close()
+	var data map[string]string
+	decoder := json.NewDecoder(document)
+	err = decoder.Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+func removeDoc(name string, dir string) {
+	if fileExists("documents/" + dir + "/" + name + ".json") {
+		os.Remove("documents/" + dir + "/" + name + ".json")
+	}
+}
+func readKey(key string, dir string) string {
+	if hasKey(key, dir) {
+		file, err := os.Open(dir + "/" + key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ret := (string(data))
+		result, err := DecryptGCM(crypto, []byte(ret))
+		if err != nil {
+			fmt.Println(err)
+		}
+		file.Close()
+		return string(result)
+	}
+	return ""
+}
+
+func addKey(key string, value string, dir string) {
+	ciphertext, err := EncryptGCM(crypto, []byte(value))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if !hasKey(key, dir) {
+		os.Mkdir(dir, os.ModePerm)
+		newFile, err = os.Create(dir + "/" + key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(newFile)
+		newFile.Close()
+		err2 := ioutil.WriteFile(dir+"/"+key, []byte(ciphertext), 0666)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+	}
+
+}
+func removeKey(key string, dir string) {
+	if hasKey(key, dir) {
+		err := os.Remove(dir + "/" + key)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
+
+func changeKeyUnsafe(key string, value string, dir string) {
+	removeKey(key, dir)
+	addKeyUnsafe(key, value, dir)
+}
+
+func changeKey(key string, value string, dir string) {
+	removeKey(key, dir)
+	addKey(key, value, dir)
+}
+
+func hasKey(key string, dir string) bool {
+	_, err := os.Stat(dir + "/" + key)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+func hasDoc(name string, dir string) bool {
+	_, err := os.Stat("documents/" + dir + "/" + name + ".json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+func addKeyUnsafe(key string, value string, dir string) {
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if !hasKey(key, dir) {
+		os.Mkdir(dir, os.ModePerm)
+		newFile, err = os.Create(dir + "/" + key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(newFile)
+		newFile.Close()
+		err2 := ioutil.WriteFile(dir+"/"+key, []byte(value), 0666)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+	}
+
+}
+func readKeyUnsafe(key string, dir string) string {
+	if hasKey(key, dir) {
+		file, err := os.Open(dir + "/" + key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ret := (string(data))
+		if err != nil {
+			fmt.Println(err)
+		}
+		file.Close()
+		return ret
+	}
+	return ""
+}
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
---
+++ src/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"github.com/xyproto/randomstring"
+)
+
+type Payment struct {
+	Acc1   string `json:"acc1" xml:"acc1" form:"acc1" query:"acc1"`
+	Pin    string `json:"pin" xml:"pin" form:"pin" query:"pin"`
+	Amount string `json:"amount" xml:"amount" form:"amount" query:"amount"`
+	Acc2   string `json:"acc2" xml:"acc2" form:"acc2" query:"acc2"`
+}
+type Account struct {
+	PIN string `json:"pin" xml:"pin" form:"pin" query:"pin"`
+}
+
+func main() {
+	e := echo.New()
+	e.POST("/pay", pay)
+	e.POST("/addAccount", addAccount)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func pay(c echo.Context) error {
+	paymentData := new(Payment)
+	if err := c.Bind(paymentData); err != nil {
+		return err
+	}
+	if hasDoc(paymentData.Acc1, "") && hasDoc(paymentData.Acc2, "") {
+		res, err := readDocUnsafe(paymentData.Acc1, "")
+		if err != nil {
+			return c.String(http.StatusCreated, "Server Error")
+		}
+		if res["pin"] == paymentData.Pin {
+			amount, err := strconv.Atoi(paymentData.Amount)
+			if err != nil {
+				return c.String(http.StatusCreated, "Server Error")
+			}
+			balance, err := strconv.Atoi(res["balance"])
+			if err != nil {
+				return c.String(http.StatusCreated, "Server Error")
+			}
+			if balance >= amount {
+				balance -= amount
+			} else {
+				return c.String(http.StatusCreated, "Not enough money")
+			}
+			addDocUnsafe(map[string]string{"balance": strconv.Itoa(balance), "pin": res["pin"]}, paymentData.Acc1, "")
+			res, err = readDocUnsafe(paymentData.Acc2, "")
+			if err != nil {
+				return c.String(http.StatusCreated, "Server Error")
+			}
+			balance, err = strconv.Atoi(res["balance"])
+			if err != nil {
+				return c.String(http.StatusCreated, "Server Error")
+			}
+			balance += amount
+			addDocUnsafe(map[string]string{"balance": strconv.Itoa(balance), "pin": res["pin"]}, paymentData.Acc2, "")
+			return c.String(http.StatusCreated, "success")
+			return c.String(http.StatusCreated, "wrong pin")
+		}
+		return c.String(http.StatusCreated, "Failed")
+	}
+	return c.String(http.StatusCreated, "Failed")
+}
+
+func addAccount(c echo.Context) error {
+	accountData := new(Account)
+	if err := c.Bind(accountData); err != nil {
+		return c.String(http.StatusCreated, "Server Error")
+	}
+	if len(accountData.PIN) != 4 {
+		return c.String(http.StatusCreated, "Bad Pin")
+	}
+	acc := map[string]string{
+		"balance": "0",
+		"pin":     accountData.PIN,
+	}
+	adress := randomstring.CookieFriendlyString(14)
+	if hasKey(adress, "") {
+		return c.String(http.StatusCreated, "already exists")
+	}
+	err := addDocUnsafe(acc, adress, "")
+	if err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusCreated, "couldnt add doc")
+	}
+	return c.String(http.StatusCreated, "success: "+adress)
+}
