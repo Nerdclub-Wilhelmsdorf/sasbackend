5abb91be57169a223b547b6b647a0213a37d6252
Len Blum <mail@lenblum.de>
2024-02-16 20:39:32 +0100

    fixes

--- src/main.go
+++ src/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -13,12 +15,18 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "0")
 	})
+	f, err := os.OpenFile("logfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic(fmt.Sprintf("error opening file: %v", err))
+	}
+	defer f.Close()
 	e.POST("/addAccount", addAccount)
 	e.POST("/balanceCheck", balanceCheck)
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "method=${method}, uri=${uri}, status=${status}\n",
+		Output: f,
 	}))
 	e.Use(middleware.CORS())
 	//e.Use(middleware.Secure())
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.StartAutoTLS(":443"))
 }
