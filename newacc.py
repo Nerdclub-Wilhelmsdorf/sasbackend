a2ec81dc6316b13056d5f318a8a0c195eeac5b91
Len <mail@lenblum.de>
2024-03-04 15:27:03 +0100

    Secure generation of passwords using secrets package

--- newacc.py
+++ newacc.py
@@ -1,6 +1,6 @@
 import sys
 import bcrypt
-import random
+import secrets
 import string
 import json
 if len(sys.argv) > 2:
@@ -8,8 +8,8 @@ if len(sys.argv) > 2:
     name = sys.argv[2]
     hashed = bcrypt.hashpw(pin.encode('utf-8'), bcrypt.gensalt())
     #print(hashed.decode('utf-8'))
-    #write pin and name to file named after random id of length 16
-    id = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+    #write pin and name to file named after random id with 16 characters
+    id = ''.join(secrets.choice(string.ascii_uppercase + string.ascii_lowercase  + string.digits) for _ in range(16))
     with open("documents/"+ id+ ".json", 'w') as file:
         json.dump({"name": name, "pin": hashed.decode('utf-8'), "balance" : "0"}, file)
     print("Account created with id: " + id)
