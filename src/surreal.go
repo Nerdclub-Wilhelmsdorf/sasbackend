29dae37ab20d24b41a24254f92cc2880e0ecdfb0
mail@lenblum.de <mail@lenblum.de>
2024-04-21 14:31:21 +0200

    Co-authored-by: sebastian.oelhaf@gmx.de <sebastian.oelhaf@gmx.de> Co-authored-by: Urs <Ursus24@users.noreply.github.com>

---
+++ src/surreal.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/surrealdb/surrealdb.go"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func test() {
+	// Create a new account
+	user := Account{
+		Name:    "203876",
+		Balance: "1000",
+		Pin:     "1234",
+	}
+	err := createAccount(user)
+	if err != nil {
+		panic(err)
+	}
+
+	// Check balance
+	balance, err := balanceCheck(BalanceCheck{
+		ID:  "user:36xxj2a4al7ybid1j7pd",
+		Pin: "1234",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Balance:", balance)
+
+	// Transfer money
+	transfer := Transfer{
+		From:   "user:36xxj2a4al7ybid1j7pd",
+		To:     "user:36xxj2a4al7ybid1j7pd",
+		Amount: "100",
+		Pin:    "1234",
+	}
+	err = transferMoney(transfer)
+	if err != nil {
+		panic(err)
+	}
+
+	// Check balance
+	balance, err = balanceCheck(BalanceCheck{
+		ID:  "user:36xxj2a4al7ybid1j7pd",
+		Pin: user.Pin,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Balance:", balance)
+}
+
+type Account struct {
+	ID      string `json:"id,omitempty"`
+	Name    string `json:"name"`
+	Balance string `json:"balance"`
+	Pin     string `json:"pin"`
+}
+
+type Transfer struct {
+	From   string `json:"from"`
+	To     string `json:"to"`
+	Amount string `json:"amount"`
+	Pin    string `json:"pin"`
+}
+
+type BalanceCheck struct {
+	ID  string `json:"id"`
+	Pin string `json:"pin"`
+}
+
+func createAccount(user Account) error {
+	db, _ := surrealdb.New("ws://localhost:8000/rpc")
+	defer db.Close()
+	if _, err := db.Use("user", "user"); err != nil {
+		return err
+	}
+	// Insert user
+	data, err := db.Create("user", user)
+	if err != nil {
+		return err
+	}
+	createdUser := make([]Account, 1)
+	err = surrealdb.Unmarshal(data, &createdUser)
+	if err != nil {
+		return err
+	}
+
+	// Get user by ID
+	data, err = db.Select(createdUser[0].ID)
+	if err != nil {
+		return err
+	}
+	selectedUser := new(Account)
+	err = surrealdb.Unmarshal(data, &selectedUser)
+	if err != nil {
+		return err
+	}
+	if user.Name == selectedUser.Name {
+		fmt.Println("User with ID: " + selectedUser.ID + " created successfully.")
+		return nil
+	} else {
+		return errors.New("failed to create user")
+	}
+
+}
+
+func transferMoney(transfer Transfer) error {
+	db, _ := surrealdb.New("ws://localhost:8000/rpc")
+	defer db.Close()
+	if _, err := db.Use("user", "user"); err != nil {
+		return err
+	}
+	data, err := db.Select(transfer.From)
+	if err != nil {
+		return err
+	}
+	acc1 := new(Account)
+	err = surrealdb.Unmarshal(data, &acc1)
+	if err != nil {
+		return err
+	}
+	if !CheckPasswordHash(transfer.Pin, acc1.Pin) {
+		return errors.New("incorrect pin")
+	}
+	amount, err := strconv.ParseFloat(transfer.Amount, 64)
+	if err != nil {
+		return err
+	}
+	balance, err := strconv.ParseFloat(acc1.Balance, 64)
+	if err != nil {
+		return err
+	}
+	if amount <= 0 {
+		return errors.New("invalid amount")
+	}
+	if amount*1.1 >= balance {
+		return errors.New("insufficient funds")
+	}
+	changes := map[string]string{"balance": fmt.Sprintf("%f", balance-amount*1.1)}
+	if _, err = db.Change(transfer.From, changes); err != nil {
+		return err
+	}
+	if _, err = db.Update(transfer.From, changes); err != nil {
+		return err
+	}
+	data, err = db.Select(transfer.To)
+	if err != nil {
+		return err
+	}
+	acc2 := new(Account)
+	err = surrealdb.Unmarshal(data, &acc2)
+	if err != nil {
+		return err
+	}
+	changes = map[string]string{"balance": fmt.Sprintf("%f", amount+balance)}
+	if _, err = db.Change(transfer.To, changes); err != nil {
+		return err
+	}
+	if _, err = db.Update(transfer.To, changes); err != nil {
+		return err
+	}
+	data, err = db.Select("zentralbank")
+	if err != nil {
+		return err
+	}
+	acc3 := new(Account)
+	err = surrealdb.Unmarshal(data, &acc3)
+	if err != nil {
+		return err
+	}
+	changes = map[string]string{"balance": fmt.Sprintf("%f", amount*0.1+balance)}
+	if _, err = db.Change("zentralbank", changes); err != nil {
+		return err
+	}
+	if _, err = db.Update("zentralbank", changes); err != nil {
+		return err
+	}
+
+	return nil
+}
+func balanceCheck(account BalanceCheck) (string, error) {
+	db, err := surrealdb.New("ws://localhost:8000/rpc")
+	if err != nil {
+		return "", fmt.Errorf("failed to connect to database: %w", err)
+	}
+	defer db.Close()
+
+	if _, err := db.Use("user", "user"); err != nil {
+		return "", fmt.Errorf("failed to use database: %w", err)
+	}
+
+	data, err := db.Select(account.ID)
+	if err != nil {
+		return "", fmt.Errorf("failed to select account with ID %s: %w", account.ID, err)
+	}
+
+	acc := new(Account)
+	err = surrealdb.Unmarshal(data, &acc)
+	if err != nil {
+		fmt.Println(data)
+		return "", fmt.Errorf("failed to unmarshal account data: %w", err)
+	}
+	if !CheckPasswordHash(account.Pin, acc.Pin) {
+		return "", errors.New("incorrect pin")
+	}
+	return acc.Balance, nil
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
